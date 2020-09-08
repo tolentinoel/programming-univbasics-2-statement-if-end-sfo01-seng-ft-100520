49cce29de12ceb46fc64f4f73dd1c8761aaa124d
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-08 19:27:47 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,9 @@
 # Write your solution here
-if true
-  puts "This code runs!"
+
+run_code_inside = true
+
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
 end
+puts "Code after if...end"
