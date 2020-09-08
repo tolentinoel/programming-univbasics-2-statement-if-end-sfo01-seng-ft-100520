6904943687bfb36f439e1ef5225efd4f2e1aaa76
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-08 20:25:14 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,12 +1,12 @@
 # Write your solution here
 
-run_code_inside = false
+#run_code_inside = false
 
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
+#puts "Code before if...end"
+#if run_code_inside
+ # puts "code inside"
+#end
+#puts "Code after if...end"
 
 
 chance_of_rain = 0.0000001
