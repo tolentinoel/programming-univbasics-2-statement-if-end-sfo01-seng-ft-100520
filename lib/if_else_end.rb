4bddaa4b1d49ea684d8ea2ee84acfc09defd4022
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-08 20:04:27 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -11,11 +11,12 @@ puts "Code after if...end"
 
 chance_of_rain = 0.2
 
-puts "Let's go outside!"
+#puts "Let's go outside!"
 
-if chance_of_rain > 0.5
-  puts "Pack an umbrella!"
-else
-  puts "Enjoy the fine day!"
-end
-puts "Oh, and always wear sunscreen!"
+#if chance_of_rain > 0.5
+#  puts "Pack an umbrella!"
+#else
+ # puts "Enjoy the fine day!"
+#end
+#puts "Oh, and always wear sunscreen!"
+
