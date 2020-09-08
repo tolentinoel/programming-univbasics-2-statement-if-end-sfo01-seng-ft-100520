2d2ec8b82cb4da2a13254f95eaddf4c2a13d6f35
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-08 20:32:24 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -27,3 +27,7 @@ elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
 else
   puts "Stay home and read Hegel."
 end
+
+puts "You know what year it is??"
+this_year = 2019
+puts "Hey, it's 2019!" if this_year == 2019
